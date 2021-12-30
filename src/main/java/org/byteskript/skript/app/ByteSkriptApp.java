031aa8e13598062fa89ba6d0dab68334a5f14fdc
Moderocky <admin@moderocky.com>
2021-12-30 11:32:00 +0000

    Clean-up unused colours.

--- src/main/java/org/byteskript/skript/app/ByteSkriptApp.java
+++ src/main/java/org/byteskript/skript/app/ByteSkriptApp.java
@@ -23,7 +23,7 @@ public class ByteSkriptApp extends SkriptApp {
     public static void main(String... args) throws Throwable {
         if (args.length < 1) {
             makeFiles();
-            System.out.println(RESET + "Welcome to " + MAGENTA_BRIGHT + "ByteSkript" + RESET + "!");
+            System.out.println(RESET + "Welcome to " + BRIGHT_PURPLE + "ByteSkript" + RESET + "!");
             System.out.println(RESET + "Available arguments:");
             System.out.println(RESET + "\trun        | " + CYAN + "Run scripts in the " + CYAN_UNDERLINED + "skripts/" + CYAN + " directory.");
             System.out.println(RESET + "\trun <file> | " + CYAN + "Run a single script in isolation.");
@@ -83,67 +83,8 @@ public class ByteSkriptApp extends SkriptApp {
         BLUE("\033[0;34m"),
         PURPLE("\033[0;35m"),
         CYAN("\033[0;36m"),
-        WHITE("\033[0;37m"),
-        
-        // Bold
-        BLACK_BOLD("\033[1;30m"),   // BLACK
-        RED_BOLD("\033[1;31m"),     // RED
-        GREEN_BOLD("\033[1;32m"),   // GREEN
-        YELLOW_BOLD("\033[1;33m"),  // YELLOW
-        BLUE_BOLD("\033[1;34m"),    // BLUE
-        MAGENTA_BOLD("\033[1;35m"), // MAGENTA
-        CYAN_BOLD("\033[1;36m"),    // CYAN
-        WHITE_BOLD("\033[1;37m"),   // WHITE
-        
-        // Underline
-        BLACK_UNDERLINED("\033[4;30m"),     // BLACK
-        RED_UNDERLINED("\033[4;31m"),       // RED
-        GREEN_UNDERLINED("\033[4;32m"),     // GREEN
-        YELLOW_UNDERLINED("\033[4;33m"),    // YELLOW
-        BLUE_UNDERLINED("\033[4;34m"),      // BLUE
-        MAGENTA_UNDERLINED("\033[4;35m"),   // MAGENTA
-        CYAN_UNDERLINED("\033[4;36m"),      // CYAN
-        WHITE_UNDERLINED("\033[4;37m"),     // WHITE
-        
-        // Background
-        BLACK_BACKGROUND("\033[40m"),   // BLACK
-        RED_BACKGROUND("\033[41m"),     // RED
-        GREEN_BACKGROUND("\033[42m"),   // GREEN
-        YELLOW_BACKGROUND("\033[43m"),  // YELLOW
-        BLUE_BACKGROUND("\033[44m"),    // BLUE
-        MAGENTA_BACKGROUND("\033[45m"), // MAGENTA
-        CYAN_BACKGROUND("\033[46m"),    // CYAN
-        WHITE_BACKGROUND("\033[47m"),   // WHITE
-        
-        // High Intensity
-        BLACK_BRIGHT("\033[0;90m"),     // BLACK
-        RED_BRIGHT("\033[0;91m"),       // RED
-        GREEN_BRIGHT("\033[0;92m"),     // GREEN
-        YELLOW_BRIGHT("\033[0;93m"),    // YELLOW
-        BLUE_BRIGHT("\033[0;94m"),      // BLUE
-        MAGENTA_BRIGHT("\033[0;95m"),   // MAGENTA
-        CYAN_BRIGHT("\033[0;96m"),      // CYAN
-        WHITE_BRIGHT("\033[0;97m"),     // WHITE
-        
-        // Bold High Intensity
-        BLACK_BOLD_BRIGHT("\033[1;90m"),    // BLACK
-        RED_BOLD_BRIGHT("\033[1;91m"),      // RED
-        GREEN_BOLD_BRIGHT("\033[1;92m"),    // GREEN
-        YELLOW_BOLD_BRIGHT("\033[1;93m"),   // YELLOW
-        BLUE_BOLD_BRIGHT("\033[1;94m"),     // BLUE
-        MAGENTA_BOLD_BRIGHT("\033[1;95m"),  // MAGENTA
-        CYAN_BOLD_BRIGHT("\033[1;96m"),     // CYAN
-        WHITE_BOLD_BRIGHT("\033[1;97m"),    // WHITE
-        
-        // High Intensity backgrounds
-        BLACK_BACKGROUND_BRIGHT("\033[0;100m"),     // BLACK
-        RED_BACKGROUND_BRIGHT("\033[0;101m"),       // RED
-        GREEN_BACKGROUND_BRIGHT("\033[0;102m"),     // GREEN
-        YELLOW_BACKGROUND_BRIGHT("\033[0;103m"),    // YELLOW
-        BLUE_BACKGROUND_BRIGHT("\033[0;104m"),      // BLUE
-        MAGENTA_BACKGROUND_BRIGHT("\033[0;105m"),   // MAGENTA
-        CYAN_BACKGROUND_BRIGHT("\033[0;106m"),      // CYAN
-        WHITE_BACKGROUND_BRIGHT("\033[0;107m");     // WHITE
+        CYAN_UNDERLINED("\033[4;36m"),
+        BRIGHT_PURPLE("\033[0;95m");
         
         private final String code;
         
