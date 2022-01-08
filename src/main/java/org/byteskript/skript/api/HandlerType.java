7252d8ed338c58d6d4b7227b0db3422ceb9701d5
Moderocky <admin@moderocky.com>
2022-01-08 15:57:25 +0000

    Remove unnecessary classes.

--- src/main/java/org/byteskript/skript/api/HandlerType.java
+++ src/main/java/org/byteskript/skript/api/HandlerType.java
@@ -6,6 +6,12 @@
 
 package org.byteskript.skript.api;
 
+/**
+ * A handler mode for interacting with expressions.
+ * The defaults (get/set/add/delete/...) are detailed in {@link org.byteskript.skript.lang.handler.StandardHandlers}.
+ *
+ * Special implementations could add behaviour like COMPARE_AND_SWAP or GET_ATOMIC.
+ */
 public interface HandlerType {
     
     String name();
